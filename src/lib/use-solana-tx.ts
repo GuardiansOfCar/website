c759ee5ce4b000f832ea3b1ac8428be70f1ef756
Jin Park <jinpk@users.noreply.github.com>
2025-03-12 12:42:14 +0900

    fix

--- src/lib/use-solana-tx.ts
+++ src/lib/use-solana-tx.ts
@@ -44,22 +44,19 @@ export function useSolanaTx(solana: any | undefined) {
           }),
     );
 
-    const res = await solana.signAndSendTransaction(transaction);
-    return res?.signature || res;
-    /*
-        const signedTransaction = await solana.signTransaction(transaction);
-    
-        const txRes = await fetch(`/api/solana`, {
-          method: "POST",
-          body: JSON.stringify({ payload: signedTransaction.serialize() }),
-        });
-    
-        if (txRes.status != 200 && txRes.status !== 201) {
-          throw new Error("Failed to broadcast transaction.");
-        }
-    
-        const { tx } = await txRes.json();
-        return tx;*/
+    const signedTransaction = await solana.signTransaction(transaction);
+
+    const txRes = await fetch(`/api/solana`, {
+      method: "POST",
+      body: JSON.stringify({ payload: signedTransaction.serialize() }),
+    });
+
+    if (txRes.status != 200 && txRes.status !== 201) {
+      throw new Error("Failed to broadcast transaction.");
+    }
+
+    const { tx } = await txRes.json();
+    return tx;
   }
 
   return {
