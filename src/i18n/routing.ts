cf926b8cc9cf5a10d42bf60040273eaf563cecb0
Jin Park <jinpk@users.noreply.github.com>
2025-03-17 20:24:24 +0900

    feat: apply lang

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,7 +2,25 @@ import { defineRouting } from "next-intl/routing";
 import { createNavigation } from "next-intl/navigation";
 
 export const routing = defineRouting({
-  locales: ["en", "ko", "ar", "tr"],
+  locales: [
+    "en",
+    "ko",
+    "ar",
+    "tr",
+    "de",
+    "es",
+    "fr",
+    "id",
+    "it",
+    "ja",
+    "nl",
+    "pt",
+    "ru",
+    "th",
+    "vi",
+    "zh-CN",
+    "zh-TW",
+  ],
   defaultLocale: "en",
 });
 
