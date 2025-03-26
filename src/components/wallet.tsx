29d9762e59e6651f1f283653527a59b65b1b38bf
Jin Park <jinpk@users.noreply.github.com>
2025-03-26 21:06:36 +0900

    f

--- src/components/wallet.tsx
+++ src/components/wallet.tsx
@@ -6,6 +6,7 @@ import { useTrustWallet } from "@/lib/use-trust-wallet";
 import { useWalletConnect } from "@/lib/use-wallet-connect";
 import { useWallet } from "@/lib/use-wallet";
 import { wrapWindow } from "@/lib/constants";
+import { useRef } from "react";
 
 export const Wallet = ({
   type,
@@ -19,6 +20,11 @@ export const Wallet = ({
   // const phantom = usePhantom();
   const trustWallet = useTrustWallet();
   const walletConnect = useWalletConnect();
+
+  const isMobile = useRef(
+    /iPhone|iPad|iPod|Android/i.test(navigator.userAgent),
+  ).current;
+
   const handleClick = () => {
     if (type === "metamask") {
       if (create) {
@@ -49,6 +55,7 @@ export const Wallet = ({
 
   if (wallet.network === "SOL" && type === "metamask") return null;
   if (type === "phantom") return null;
+  if (isMobile && type == "trust") return null;
 
   return (
     <button
