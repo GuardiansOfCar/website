2eea9c40153e5ff5d588b55bf7ced963288a434f
wonkee-choi <wonkic@gmail.com>
2025-08-13 14:19:28 +0900

    feat: next config, amplify

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,11 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    // 경고: 타입 에러가 있어도 프로덕션 빌드를 강제로 성공시킵니다.
+    // 로컬에서 충분히 타입을 검증해야 합니다.
+    ignoreBuildErrors: true,
+  },
   webpack(config) {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     config.module.rules.push({
